4e12a819b8d2a1a662ebf7ccafbf1a6c4dadd52f
WLHeyneke <69819966+WLHeyneke@users.noreply.github.com>
2024-08-02 13:24:53 +0200

    screen sizing breakpoints

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,34 +40,61 @@ const blurHeader = () => {
 window.addEventListener("scroll", blurHeader);
 
 /*=============== EMAIL JS ===============*/
-const contactForm = document.getElementById("contact__form"),
-  contactMessage = document.getElementById("contact__message");
+const contactForm = document.getElementById("contact-form"),
+  contactMessage = document.getElementById("contact-message");
 
 const sendEmail = (e) => {
   e.preventDefault();
 
   // serviceID - templateID - #form - publicKey
-  emailjs.sendEmail(
-      "service_4enakae",
-      "template_mii3udk",
-      "#contact-form",
-      "4nVtjP5y47mWR-F7w"
-    )
-    .then(
-      () => {
+  emailjs.send('service_4enakae','template_h0il9mm','new FormData(contactForm)', '4nVtjP5y47mWR-F7w')
+    .then(() => {
         //show sent message
-        contactMessage.textContent = "Message sent successfully ✅";
-      },
-      () => {
+        contactMessage.textContent = 'Message sent successfully ✅';
+
+        //remove message after five seconds
+        setTimeout(() =>{
+          contactMessage.textContent = ' '
+        }, 5000)
+
+        //Clear the input fields
+        contactForm.reset()
+
+      },() => {
         //show error message
-        contactMessage.textContent = "Message not sent (service error) ❌";
+        contactMessage.textContent = 'Message not sent (service error) ❌';
       }
     );
 };
 
-contactForm.addEventListener("submit", sendEmail);
+contactForm.addEventListener('submit',sendEmail);
 /*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () =>{
+        const scrollUp = document.getElementById('scroll-up')
+  //Add the show-scroll class to the tag with scrollup when the scroll is higher than 350 viewport height
+        this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                                                : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const section = document.querySelectorAll('section[id]')
+
+const scrollActive = () =>{
+  const scrollY = window.pageYOffset
+
+  SpeechRecognitionResult.forEach(current =>{
+    const sectionHeight = current.offsetHeight,
+          sectionTop = current.offsetTop -58,
+          sectionId = current.getAttribute('id'),
+          sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            sectionsClass.classList.add('active-link')
+    }else{
+            sectionsClass.classList.remove('active-link')
+    }
+  })
+}
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
